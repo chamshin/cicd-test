246f70da87a7ccfe7decaa01e63dc27adb22c342
hub65536 <study65536@gmail.com>
2022-03-15 14:14:50 +0900

    Add index controller

---
+++ src/main/java/cicd/test/controller/Index.java
@@ -0,0 +1,14 @@
+package cicd.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Index {
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello world";
+    }
+
+}
